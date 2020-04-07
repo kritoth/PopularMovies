2c3a8628d6402becee53d262515db1b52dca1f8b
kritoth <kritoth@gmail.com>
2020-04-07 15:16:54 +0200

    Set up adapter to RecyclerView

--- app/src/main/java/com/tiansirk/popularmovies/MainActivity.java
+++ app/src/main/java/com/tiansirk/popularmovies/MainActivity.java
@@ -1,17 +1,24 @@
 package com.tiansirk.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private TextView mErrorMessage;
     private ProgressBar mLoadingIndicator;
+
+    private MovieAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +29,34 @@ public class MainActivity extends AppCompatActivity {
         mErrorMessage = findViewById(R.id.tv_error_message_display);
         mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
 
-        //Dummy data for testing
-        String imgPath = "http://i.imgur.com/DvpvklR.png";
-        String overview = "This film would be great if it did exist.";
-        String releaseDate = "2020-04-07";
-        String originalTitle = "The Dummy Movie";
-        double voteAvg = 9.87;
-        Movie dummyMovie = new Movie(imgPath, overview, releaseDate, originalTitle, voteAvg);
-        
+        RecyclerView.LayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new MovieAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        showDataView();
+    }
+
+    /**
+     * This method will make the RecyclerView visible and hide the error message
+     */
+    private void showDataView() {
+        /* First, make sure the error is invisible */
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        /* Then, make sure the weather data is visible */
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the RecyclerView
+     */
+    private void showErrorMessage() {
+        /* First, hide the currently visible data */
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        /* Then, show the error */
+        mErrorMessage.setVisibility(View.VISIBLE);
     }
 }
