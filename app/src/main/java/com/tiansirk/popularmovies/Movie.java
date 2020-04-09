48023b9230dc690eca9eafbf554e206011e55634
kritoth <kritoth@gmail.com>
2020-04-09 15:13:42 +0200

    Implement Parcelable in Movie

--- app/src/main/java/com/tiansirk/popularmovies/Movie.java
+++ app/src/main/java/com/tiansirk/popularmovies/Movie.java
@@ -1,17 +1,56 @@
 package com.tiansirk.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
 /**
  * Class for Movie objects to be presented to the user.
  */
-public class Movie {
+public class Movie implements Parcelable {
     private String posterImgUrl;
     private String plotSynopsis;
     private String releaseDate;
     private String title;
     private double userRating;
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    /**
+     * Write object's data to the passed-in Parcel
+     */
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(posterImgUrl);
+        parcel.writeString(plotSynopsis);
+        parcel.writeString(releaseDate);
+        parcel.writeString(title);
+        parcel.writeDouble(userRating);
+    }
+    /**
+     * This is used to regenerate Movie object. All Parcelables must have a CREATOR that implements these two methods
+      */
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    // Parcelling part of constructor. Must maintain the same order of the fields
+    public Movie(Parcel in){
+        this.posterImgUrl = in.readString();
+        this.plotSynopsis = in.readString();
+        this.releaseDate = in.readString();
+        this.title = in.readString();
+        this.userRating = in.readDouble();
+    }
 
     public Movie(String posterPath, String overview, String releaseDate, String originalTitle, double voteAverage){
         this.posterImgUrl = posterPath;
@@ -55,4 +94,5 @@ public class Movie {
                 "\n Release date: " + getReleaseDate() +
                 "\n User rating: " + getUserRating();
     }
+
 }
