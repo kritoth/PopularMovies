4a3104319d816eddf3195677887aafc41cfcd668
kritoth <kritoth@gmail.com>
2020-05-29 19:45:39 +0200

    Add background thread executor

--- app/src/main/java/com/tiansirk/popularmovies/FavoriteActivity.java
+++ app/src/main/java/com/tiansirk/popularmovies/FavoriteActivity.java
@@ -12,6 +12,7 @@ import com.tiansirk.popularmovies.data.FavoriteMovie;
 import com.tiansirk.popularmovies.data.Movie;
 import com.tiansirk.popularmovies.data.Review;
 import com.tiansirk.popularmovies.data.VideoKey;
+import com.tiansirk.popularmovies.util.AppExecutors;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +22,6 @@ public class FavoriteActivity extends AppCompatActivity {
     private static final String TAG = FavoriteActivity.class.getSimpleName();
 
     private AppDatabase mDbase;
-    List<FavoriteMovie> mFavoriteMovies;
     private List<Movie> mMovies;
 
     @Override
@@ -32,32 +32,37 @@ public class FavoriteActivity extends AppCompatActivity {
         mMovies = new ArrayList<>();
 
         mDbase = AppDatabase.getsInstance(this);
-        mFavoriteMovies = new ArrayList<>();
-        mFavoriteMovies.addAll(mDbase.movieDAO().loadAllFavMoviesByRating());
-        List<String> reviews = new ArrayList<>();
-        List<String> trailers = new ArrayList<>();
+        final List<FavoriteMovie> favoriteMovies = new ArrayList<>();
+        favoriteMovies.addAll(mDbase.movieDAO().loadAllFavMoviesByRating());
+        final List<String> reviews = new ArrayList<>();
+        final List<String> trailers = new ArrayList<>();
 
-        for(FavoriteMovie f : mFavoriteMovies){
-            if(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()) != null) {
-                List<Review> currMovieReviews = new ArrayList<Review>();
-                currMovieReviews.addAll(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()));
-                for(Review r : currMovieReviews) {
-                    reviews.add(r.getReview());
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                for(FavoriteMovie f : favoriteMovies){
+                    if(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()) != null) {
+                        List<Review> currMovieReviews = new ArrayList<Review>();
+                        currMovieReviews.addAll(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()));
+                        for(Review r : currMovieReviews) {
+                            reviews.add(r.getReview());
+                        }
+                    }
+                    if(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()) != null) {
+                        List<VideoKey> currMovieTrailers = new ArrayList<VideoKey>();
+                        currMovieTrailers.addAll(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()));
+                        for(VideoKey v : currMovieTrailers) {
+                            trailers.add(v.getVideoKey());
+                        }
+                    }
+                    mMovies.add(new Movie(f.getPosterImgUrl(), f.getPlotSynopsis(), f.getReleaseDate(), f.getTitle(),
+                            f.getUserRating(), trailers, reviews, f.getOnlineId()));
                 }
+                Log.d(TAG, "No. of Reviews in DB: " + mDbase.reviewDAO().getReviewsCount());
+                Log.d(TAG, "No. of Trailers in DB: " + mDbase.trailerDAO().getTrailersCount());
             }
-            if(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()) != null) {
-                List<VideoKey> currMovieTrailers = new ArrayList<VideoKey>();
-                currMovieTrailers.addAll(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()));
-                for(VideoKey v : currMovieTrailers) {
-                    trailers.add(v.getVideoKey());
-                }
-            }
+        });
 
-            mMovies.add(new Movie(f.getPosterImgUrl(), f.getPlotSynopsis(), f.getReleaseDate(), f.getTitle(), f.getUserRating(),
-                    trailers, reviews, f.getOnlineId()));
-        }
-        Log.d(TAG, "No. of Reviews in DB: " + mDbase.reviewDAO().getReviewsCount());
-        Log.d(TAG, "No. of Trailers in DB: " + mDbase.trailerDAO().getTrailersCount());
 
 
     }
