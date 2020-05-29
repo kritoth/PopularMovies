3f4466891d2f274de80a469197aeab6b7731909a
kritoth <kritoth@gmail.com>
2020-05-29 16:57:24 +0200

    Correct reading from DBASE in FavoriteActivity

--- app/src/main/java/com/tiansirk/popularmovies/FavoriteActivity.java
+++ app/src/main/java/com/tiansirk/popularmovies/FavoriteActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.tiansirk.popularmovies.data.AppDatabase;
 import com.tiansirk.popularmovies.data.FavoriteMovie;
@@ -21,7 +22,7 @@ public class FavoriteActivity extends AppCompatActivity {
 
     private AppDatabase mDbase;
     List<FavoriteMovie> mFavoriteMovies;
-    List<Movie> mMovies;
+    private List<Movie> mMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,37 +38,29 @@ public class FavoriteActivity extends AppCompatActivity {
         List<String> trailers = new ArrayList<>();
 
         for(FavoriteMovie f : mFavoriteMovies){
-            if(mDbase.reviewDAO().loadReview(f.getOnlineId()) != null) {
-                reviews.add(mDbase.reviewDAO().loadReview(f.getOnlineId()).getReview());
+            if(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()) != null) {
+                List<Review> currMovieReviews = new ArrayList<Review>();
+                currMovieReviews.addAll(mDbase.reviewDAO().loadReviewsByMovie(f.getOnlineId()));
+                for(Review r : currMovieReviews) {
+                    reviews.add(r.getReview());
+                }
             }
-            if(mDbase.trailerDAO().loadTrailer(f.getOnlineId()) != null) {
-                trailers.add(mDbase.trailerDAO().loadTrailer(f.getOnlineId()).getVideoKey());
+            if(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()) != null) {
+                List<VideoKey> currMovieTrailers = new ArrayList<VideoKey>();
+                currMovieTrailers.addAll(mDbase.trailerDAO().loadTrailersByMovie(f.getOnlineId()));
+                for(VideoKey v : currMovieTrailers) {
+                    trailers.add(v.getVideoKey());
+                }
             }
+
             mMovies.add(new Movie(f.getPosterImgUrl(), f.getPlotSynopsis(), f.getReleaseDate(), f.getTitle(), f.getUserRating(),
                     trailers, reviews, f.getOnlineId()));
         }
+        Log.d(TAG, "No. of Reviews in DB: " + mDbase.reviewDAO().getReviewsCount());
+        Log.d(TAG, "No. of Trailers in DB: " + mDbase.trailerDAO().getTrailersCount());
 
-        Log.d(TAG, "Request from Database: " + printMovies(reviews, trailers));
+
     }
 
-    private String printMovies(List<String> reviews, List<String> videoKeys) {
-        String s = "\nNo. of Movies = " + mFavoriteMovies.size()
-                + "\nNo. of FavoriteMovies = " + mFavoriteMovies.size()
-                + "\nFavoriteMovies: ";
-        String s1 = "";
-        for(int i=0; i<mFavoriteMovies.size(); i++){
-            s1 += TextUtils.concat(
-                    "\n",
-                    Integer.toString(i),
-                    ": ",
-                    mFavoriteMovies.get(i).getTitle(),
-                    "\nReviews: ",
-                    Integer.toString(reviews.size()),
-                    "\nTrailers: ",
-                    Integer.toString(videoKeys.size())).toString();
-        }
-        String s2 = "\nMovies: " + mMovies.toString();
 
-        return s+s1+s2;
-    }
 }
